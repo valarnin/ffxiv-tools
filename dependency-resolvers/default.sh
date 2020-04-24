b3674055e9580865b7fe8e82ef354515fe675620
Valarnin <valarnin@users.noreply.github.com>
2020-04-23 22:47:32 -0400

    - More adjustments to the dependency resolver code

--- dependency-resolvers/default.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RESOLVE_DEPS() {
-    echo
-    error "Please install the libraries and/or tools listed above as missing before continuing."
-    echo
-    exit 1
-}
--- dependency-resolvers/detect.sh
+++ dependency-resolvers/detect.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Default resolver, distro-specific solutions should override this
+RESOLVE_DEPS() {
+    echo
+    error "Please install the libraries and/or tools listed above as missing before continuing."
+    echo
+    exit 1
+}
+
 if [[ -e "/etc/gentoo-release" ]]; then
     . "$(dirname "${BASH_SOURCE[0]}")/gentoo.sh"
     return
 fi
-
-. "$(dirname "${BASH_SOURCE[0]}")/default.sh"
