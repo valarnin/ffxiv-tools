278005dc97b64d27b15c030b68c5e41ea7a60d8a
ChristopherJTrent <ChristopherJTrent@outlook.com>
2022-08-04 16:09:05 -0700

    refactor AUR check

--- helpers/ensure-aur-xlcore.sh
+++ helpers/ensure-aur-xlcore.sh
@@ -1,25 +1,31 @@
 . ./funcs.sh
 . ./error.sh
 
-CHECK_FOR_FLATPAK() 
-{
+CHECK_FOR_FLATPAK() {
     if command -v flatpak &>/dev/null; then
+        # dev.goats.xivlauncher is the current flatpak name for XLCore on flathub. Change this if that name changes for some reason.
         if flatpak list --app | grep "dev.goats.xivlauncher"; then
             error "Detected the flatpak version of XLCore. ACT cannot run in a flatpak environment. Please install XLCore from the AUR instead."
-        else 
+            return 1
+        else
             success "XLCore flatpak not detected... Checking for AUR build now..."
-            if command -v pacman &>/dev/null; then 
-                if pacman -Q | grep "xivlauncher" &>/dev/null; then 
-                    success "Found AUR XLCore. Please ensure that the following setting is set in your XIVLauncher UI:"
-                    echo "Settings -> Wine -> Installation Type should be \"Managed by XIVLauncher\""
-                    echo "This is generally the correct setting regardless of whether you wish to use ACT. You should not use system WINE to run ff14."
-                fi
-            fi
+            return CHECK_FOR_AUR
         fi
-
     else
-        echo "flatpak not installed"
+        warn "Flatpak is not installed. Continuing under the assumption that you cannot install a flatpak without it."
+        return CHECK_FOR_AUR
     fi
 }
 
-CHECK_FOR_FLATPAK;
+CHECK_FOR_AUR() {
+    if command -v pacman &>/dev/null; then
+        if pacman -Q | grep "xivlauncher" &>/dev/null; then
+            success "Found AUR XLCore. Please ensure that the following setting is set in your XIVLauncher UI:"
+            echo "Settings -> Wine -> Installation Type should be \"Managed by XIVLauncher\""
+            echo "This is generally the correct setting regardless of whether you wish to use ACT. You should not use system WINE to run ff14."
+            return 0
+        fi
+    fi
+}
+
+CHECK_FOR_FLATPAK
